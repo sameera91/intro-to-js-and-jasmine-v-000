7eed92024de0440d7f78e98c13cc1b5136804ac4
sameera91 <sameera.bachoti@gmail.com>
2016-05-20 11:08:01 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(iceCream){
+  return "I love " + iceCream; 
+}
+
+function shouting(sentence){
+  return sentence.toUpperCase();
+}
+
+function roundDown(number){
+  return Math.floor(number);
+}
+
+function theTruth(){
+  return true;
+}
